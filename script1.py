26a51cdece694027dbb294788eb104de8d298f06
Rajeev Verma <utopianrajeev@gmail.com>
2017-07-21 02:08:45 +0530

    Major issue resolved

--- script1.py
+++ script1.py
@@ -12,30 +12,29 @@ import runkmeans
 image = img.imread('img.png')
 image = np.array(image)
 imagem = np.reshape(image, (np.shape(image)[0]*np.shape(image)[1], np.shape(image)[2]))
-imagem = imagem/255
-##
-K = 16
-max_iters = 10
-##
-initial_centroids = initcentroids.centroids(imagem,K) #to randomly initialize centroids
-##
-#indexed = closestcentroids.index(initial_centroids, imagem)
-##
-#centroids  = closestcentroids.computecentroids(imagem, indexed, K)
+#imagem = imagem/255  #This was major issue 
+ 
+K = 30
+max_iters = 15
 
-centroids, indexed = runkmeans.run(imagem,initial_centroids, max_iters)
+initial_centroids = initcentroids.centroids(imagem,K) 
+
+centroids,indexed  = runkmeans.run(imagem,initial_centroids, max_iters)
+
+indexed = closestcentroids.index(centroids, imagem)
 
 recovered_imagem = np.zeros((indexed.shape[0], centroids.shape[1]))
+
+
 
 
 for i in range(indexed.shape[0]):
     recovered_imagem[i,:] = centroids[int(indexed[i]), :]
 
-recovered_image = np.reshape(recovered_imagem, (image.shape[0], image.shape[1],3))
+recovered_image = np.reshape(recovered_imagem, (image.shape[0], image.shape[1], image.shape[2]))
 
-np.save('imagematrix', recovered_image)
+np.save('imagematrix.npy', recovered_image)
 
-f.close()
 
 
 
